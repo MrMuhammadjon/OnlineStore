9a5832898de2ea298ec38eee6f8f139b988ca57a
MrMuhammadjon <asliyevmukhammadjon@gmail.com>
2025-05-30 14:39:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AnimatedNav from "./components/AnimatedNav.jsx";
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <AnimatedNav />
+    </div>
+  );
+}
